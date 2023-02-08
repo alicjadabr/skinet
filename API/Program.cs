8ef4613abdaa0b7a4ba2a7ae6397fa48bc3d07bc
Alicja Dabrowska <alicja.dab27@gmail.com>
2023-02-08 22:11:26 +0100

    Add Error Handling Files

--- API/Program.cs
+++ API/Program.cs
@@ -2,37 +2,35 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Core.Interfaces;
 using API.Helpers;
+using API.Middleware;
+using Microsoft.AspNetCore.Mvc;
+using API.Errors;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-builder.Services.AddAutoMapper(typeof(MappingProfiles));
-
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 // app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseStaticFiles();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
@@ -47,7 +45,7 @@ try
 }
 catch (Exception ex)
 {
-   logger.LogError(ex, "An error occured during migration");
-} 
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
